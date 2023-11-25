44d1755e0942a037b4814de9b8b10e7f8c55bc5c
Shivansh Srivastava <115993253+shivansh0017@users.noreply.github.com>
2023-11-25 19:31:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const employeeRoute = require("./controller/employeeRouter");
+const employeeRoute = require("./controller/employeeRoute");
 const cors= require("cors");
 const bodyParser= require("body-parser");
 
